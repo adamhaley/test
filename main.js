9a8afb2412b9ccc8ec5038c7b1de5ba083564e53
Adam Haley <adamhaley@gmail.com>
2016-04-21 12:44:02 -0700

    moved back to mock data, added poster images , some layout tweaks

--- main.js
+++ main.js
@@ -2,7 +2,21 @@
 	var resultsTpl = $("#results-tpl").html()
 	, $el = $("#results-container")
 	, apiUrl = 'http://api.themoviedb.org/3/search/movie?api_key=51a0abce642402e7b8d43b2081302e77'
+	, mockUrl = 'data/movies.json'
 	;
+
+	function testing(data) {
+        if (data.Error) {
+            console.dir(data);
+			console.log(data.results);
+			var html = Mustache.to_html(resultsTpl,data);
+			$el.html(html);
+        }
+        else {
+           console.log('JSONP Error');
+        }
+    }
+
 
 	$("form").on("submit",function(e){
 		e.preventDefault()
@@ -11,11 +25,11 @@
 
 		$.ajax({
 			type: 'GET',
-			url: apiUrl + '?query=' + queryText,
-			async: false,
-			jsonpCallback: 'jsonpCallback',
+			// url: apiUrl + '?query=' + queryText,
+			url: mockUrl,
+			// jsonp: 'testing',
 			contentType: 'application/json',
-			dataType: 'jsonp',
+			// dataType: 'jsonp',
 			success: function(data) {
 				console.dir(data);
 				console.log(data.results);
@@ -23,7 +37,7 @@
 				$el.html(html);
 			},
 			error: function(e) {
-				console.log(e.message);
+				console.log(e);
 			}
 
 		});
