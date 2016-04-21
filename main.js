43d9603e2babdc31ed33ef15c94429196647589e
Adam Haley <adamhaley@gmail.com>
2016-04-21 12:04:43 -0700

    grid layout, fluid boxes

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
 (function(){
 	var resultsTpl = $("#results-tpl").html()
-	, partials = {
-		resultPartial: $("#result-partial").html()
-	}
-	, $el = $("#container")
+	, $el = $("#results-container")
 	;
 
-	console.log("hi from JS");
 
 	$.get("data/movies.json",function(data){
 		console.log(data.results);
-		var html = Mustache.to_html(resultsTpl,data,partials);
+		var html = Mustache.to_html(resultsTpl,data);
 		$el.html(html);
 
 	});
